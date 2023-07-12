310f0c53ee87a625ac058026bfbecee64dd3b9b8
nandinikumarvarshney <nadiniboom@gmail.com>
2023-07-12 15:12:31 +0530

    Complete Code

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,38 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository repo;
+    public User createUser(User user){
+        return repo.save(user);
+    }
+    public List<User>getUsers(){
+        return repo.findAll();
+    }
+    public User getUserById(int userid){
+        return repo.findById(userid).orElse(null);
+    }
+    public User updateUser(User user, int userid){
+        User olduser=repo.findById(userid).orElse(null);
+        olduser.setName(user.getName());
+        olduser.setTTL(user.getTTL());
+        olduser.setTimestamp(user.getTimestamp());
+        repo.save(olduser);
+        return olduser;
+
+
+
+
+    }
+    public void deleteUser(int userid){
+        repo.deleteById(userid);
+    }
+}
